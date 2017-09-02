e948fc0f7d12741e01a2f12a9d876684711ad145
Peer Niesche <peer.niesche@aikq.de>
2017-09-02 03:17:42 +0200

    added recipe for log_rubber

--- src/main/java/de/honeypot/technica/init/ModRecipes.java
+++ src/main/java/de/honeypot/technica/init/ModRecipes.java
@@ -1,5 +1,6 @@
 package de.honeypot.technica.init;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -8,6 +9,7 @@ public class ModRecipes {
         GameRegistry.addSmelting(new ItemStack(ModBlocks.ORE_1, 1, 0), new ItemStack(ModItems.INGOT_COPPER), 0.7f);
         //TODO: Discuss the necessity of this.
         //GameRegistry.addSmelting(ModBlocks.LOG_RUBBER, new ItemStack(ModItems.RUBBER, 1),0.8f);
+        GameRegistry.addSmelting(ModBlocks.LOG_RUBBER, new ItemStack(Items.COAL, 1, 1), 0.4f);
         GameRegistry.addSmelting(new ItemStack(ModItems.RUBBER_BAND, 1), new ItemStack(ModItems.RUBBER, 1),0.2f);
         GameRegistry.addSmelting(ModItems.BOWL_RESIN, new ItemStack(ModItems.BOWL_RESIN_DRY), 0);
     }
