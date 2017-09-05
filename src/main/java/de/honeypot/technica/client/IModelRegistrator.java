0e59a6eb822f430e850a922fb0e9e9f0ebc3570b
Busti <mbust@live.de>
2017-09-05 22:35:41 +0200

    Made IModelRegistrator sided.

--- src/main/java/de/honeypot/technica/client/IModelRegistrator.java
+++ src/main/java/de/honeypot/technica/client/IModelRegistrator.java
@@ -1,5 +1,10 @@
 package de.honeypot.technica.client;
 
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
 public interface IModelRegistrator {
+    @SideOnly(Side.CLIENT)
     void registerModel();
 }
