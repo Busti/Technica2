565270d5a9ddcc879a179d556f64cf376dfb94e6
Busti <mbust@live.de>
2017-09-07 20:15:37 +0200

    Created a side sensitive ItemHandler Interface.

---
+++ src/main/java/de/honeypot/technica/inventory/ISidedItemHandler.java
@@ -0,0 +1,39 @@
+package de.honeypot.technica.inventory;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.items.IItemHandler;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public interface ISidedItemHandler extends IItemHandler {
+    /**
+     * Inserts an ItemStack into the given slot and return the remainder.
+     * The ItemStack should not be modified in this function!
+     * Note: This behaviour is subtly different from IFluidHandlers.fill()
+     *
+     * @param facing The facing the method was called from.
+     * @param slot     Slot to insert into.
+     * @param stack    ItemStack to insert.
+     * @param simulate If true, the insertion is only simulated
+     * @return The remaining ItemStack that was not inserted (if the entire stack is accepted, then return ItemStack.EMPTY).
+     *         May be the same as the input ItemStack if unchanged, otherwise a new ItemStack.
+     **/
+    @Nonnull
+    ItemStack insertItem(@Nullable EnumFacing facing, int slot, @Nonnull ItemStack stack, boolean simulate);
+
+    /**
+     * Extracts an ItemStack from the given slot. The returned value must be null
+     * if nothing is extracted, otherwise it's stack size must not be greater than amount or the
+     * itemstacks getMaxStackSize().
+     *
+     * @param facing The facing the method was called from.
+     * @param slot     Slot to extract from.
+     * @param amount   Amount to extract (may be greater than the current stacks max limit)
+     * @param simulate If true, the extraction is only simulated
+     * @return ItemStack extracted from the slot, must be ItemStack.EMPTY, if nothing can be extracted
+     **/
+    @Nonnull
+    ItemStack extractItem(@Nullable EnumFacing facing, int slot, int amount, boolean simulate);
+}
