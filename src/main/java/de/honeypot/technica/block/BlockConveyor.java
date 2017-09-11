8816f77a08c5cea95854c9bc09d4325504afb7c9
Busti <mbust@live.de>
2017-09-12 01:46:03 +0200

    Registered properties.

--- src/main/java/de/honeypot/technica/block/BlockConveyor.java
+++ src/main/java/de/honeypot/technica/block/BlockConveyor.java
@@ -9,11 +9,13 @@ import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
@@ -22,9 +24,9 @@ import java.util.Map;
 public class BlockConveyor extends Block {
     public static final String CONVEYOR = "conveyor";
 
-    public static final IProperty FACING = BlockHorizontal.FACING;
-    public static final Map<EnumFacing, IProperty> CONNECTED = new HashMap<>();
-    public static final IProperty HAS_MOTOR = PropertyBool.create("has_motor");
+    public static final IProperty<EnumFacing> FACING = BlockHorizontal.FACING;
+    public static final Map<EnumFacing, IProperty<Boolean>> CONNECTED = new HashMap<>();
+    public static final IProperty<Boolean> HAS_MOTOR = PropertyBool.create("has_motor");
 
     static {
         for (EnumFacing facing: EnumFacing.Plane.HORIZONTAL.facings()) {
@@ -32,16 +34,16 @@ public class BlockConveyor extends Block {
         }
     }
 
-    protected BlockConveyor() {
+    public BlockConveyor(String name) {
         super(Material.IRON);
-        setRegistryName("conveyor");
-        setUnlocalizedName("conveyor");
+        setRegistryName(name);
+        setUnlocalizedName(name);
         ModBlocks.registerBlock(this);
         setCreativeTab(Technica.CREATIVE_TAB_TECHNICA);
 
         ItemBlock item = new ItemBlock(this);
-        item.setRegistryName(CONVEYOR);
-        item.setUnlocalizedName(CONVEYOR);
+        item.setRegistryName(name);
+        item.setUnlocalizedName(name);
         item.setCreativeTab(Technica.CREATIVE_TAB_TECHNICA);
         ModItems.registerItem(item);
 
@@ -53,7 +55,37 @@ public class BlockConveyor extends Block {
                 .withProperty(CONNECTED.get(EnumFacing.WEST), false)
                 .withProperty(HAS_MOTOR, false)
         );
+
+        GameRegistry.registerTileEntity(TileConveyorBase.class, name);
     }
+
+    /* State Handling */
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        IProperty[] properties = {
+                FACING,
+                CONNECTED.get(EnumFacing.NORTH),
+                CONNECTED.get(EnumFacing.EAST),
+                CONNECTED.get(EnumFacing.SOUTH),
+                CONNECTED.get(EnumFacing.WEST),
+                HAS_MOTOR
+        };
+        return new BlockStateContainer(this, properties);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public IBlockState getStateFromMeta(int meta) {
+        return getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getHorizontalIndex();
+    }
+
+    /* Tile Entity */
 
     @Nullable
     @Override
